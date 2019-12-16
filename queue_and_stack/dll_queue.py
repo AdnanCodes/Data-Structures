765e36fbb96dec1664f324f7bf9fa457435c765d
Adnan <adnan.chowdhury@outlook.com>
2019-12-16 16:22:05 -0600

    Queue done and moved DLL, doesn't import with sys

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,5 +1,5 @@
-import sys
-sys.path.append('../doubly_linked_list')
+# import sys
+# sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 
@@ -7,13 +7,23 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # Doubly Linked List allows easily find the head or tail
+        # It also helps with First In and First Out Approach
+        # When enqueue we can use the tail pointer and when we dequeue we can use head pointer
+        # Since DLL has methods constructed, we can use those to implement faster
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        #To queue up, add the item to last of the linked list and increase its link by 1
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        #Make sure we are not dealing with empty Queue
+        if self.size > 0:
+            #Reduce the size and take out the item from the head (first out), returns the Value stored in that item
+            self.size -= 1
+            return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        return self.size
